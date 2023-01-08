b06d38de60f1b6b020d317271d6f92b65ae3829c
samarth <munde.samarth28@gmail.com>
2023-01-08 19:21:36 +0530

    game is done

--- PigGame/script.js
+++ PigGame/script.js
@@ -12,17 +12,23 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+let scores, currentScore, activePlayer, playing;
 // starter condition
-current0El.textContent = 0;
-current1El.textContent = 0;
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+function init() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+}
 
 function switchPlayer() {
   document.querySelector(`#current--${activePlayer}`).textContent = 0;
@@ -72,3 +78,5 @@ btnHold.addEventListener("click", function() {
     }
   }
 });
+
+btnNew.addEventListener("click", init);
